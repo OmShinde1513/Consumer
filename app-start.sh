5a3f416fc869be2f0871ec4937add0cad8c7d789
OmShinde1513 <141213612+OmShinde1513@users.noreply.github.com>
2024-02-21 00:04:15 +0530

    Update app-start.sh

--- app-start.sh
+++ app-start.sh
@@ -2,10 +2,10 @@
 mkdir /home/app
 mkdir /home/app/java
 mkdir /home/app/config
-mv consumer-app.jar /home/app/java
-mv consumer.properties /home/app/config
-mv consumer-start.sh /home/app
-mv consumer.service /etc/systemd/system
+mv -f consumer-app.jar /home/app/java
+mv -f consumer.properties /home/app/config
+mv -f consumer-start.sh /home/app
+mv -f consumer.service /etc/systemd/system
 systemd daemon reload
 systemctl enable consumer
 systemctl start consumer
